139723ce9fd0e8ba8e38bfce44b0905d7ba97034
Sanae <sanae@sanaes-air.speedport.ip>
2022-03-26 17:47:23 +0100

    add  conditions for total character cards

--- src/main.js
+++ src/main.js
@@ -25,44 +25,171 @@ Promise.all(promises).then((pages) => {
     const statusSelection = document.querySelector("select");
     cardList.innerHTML = "";
 
-    if (
+
+  /* ===== conditions for rick ===== */
+  if (
+    inputText.value.toLowerCase().includes("rick") &&
+    statusSelection.value === "alive"
+  ) {
+    const rickCharacters = characters.filter((character) => {
+      return (
+        character.name.includes("Rick") && character.status.includes("Alive")
+      );
+    });
+    rickCharacters.forEach(renderCharacter);
+  } else if (
+    inputText.value.toLowerCase().includes("rick") &&
+    statusSelection.value === "dead") {
+    const rickCharacters = characters.filter((character) => {
+      return (
+        character.name.includes("Rick") && character.status.includes("Dead")
+      );
+    });
+    rickCharacters.forEach(renderCharacter); 
+  } else if (
       inputText.value.toLowerCase().includes("rick") &&
-      statusSelection.value === "alive"
-    ) {
+      statusSelection.value === "unknown") {
       const rickCharacters = characters.filter((character) => {
         return (
-          character.name.includes("Rick") && character.status.includes("Alive")
+          character.name.includes("Rick") && character.status.includes("unknown")
         );
       });
       rickCharacters.forEach(renderCharacter);
-    } else if (
+   } else if (
       inputText.value.toLowerCase().includes("rick") &&
-      statusSelection.value === "dead") {
+      statusSelection.value === "all status") {
       const rickCharacters = characters.filter((character) => {
         return (
-          character.name.includes("Rick") && character.status.includes("Dead")
+          character.name.includes("Rick")
         );
       });
-      rickCharacters.forEach(renderCharacter); 
-    } else if (
-        inputText.value.toLowerCase().includes("rick") &&
-        statusSelection.value === "unknown") {
-        const rickCharacters = characters.filter((character) => {
-          return (
-            character.name.includes("Rick") && character.status.includes("unknown")
-          );
-        });
-        rickCharacters.forEach(renderCharacter);
-     } else if (
-        inputText.value.toLowerCase().includes("rick") &&
-        statusSelection.value === "all status") {
-        const rickCharacters = characters.filter((character) => {
-          return (
-            character.name.includes("Rick")
-          );
-        });
-        rickCharacters.forEach(renderCharacter);
+      rickCharacters.forEach(renderCharacter);
+      }
+    
+     /* ===== conditions for morty ===== */
+
+        if (
+          inputText.value.toLowerCase().includes("morty") &&
+          statusSelection.value === "alive"
+        ) {
+          const mortyCharacters = characters.filter((character) => {
+            return (
+              character.name.includes("Morty") && character.status.includes("Alive")
+            );
+          });
+          mortyCharacters.forEach(renderCharacter);
+        } else if (
+          inputText.value.toLowerCase().includes("morty") &&
+          statusSelection.value === "dead") {
+          const mortyCharacters = characters.filter((character) => {
+            return (
+              character.name.includes("Morty") && character.status.includes("Dead")
+            );
+          });
+          mortyCharacters.forEach(renderCharacter); 
+        } else if (
+            inputText.value.toLowerCase().includes("morty") &&
+            statusSelection.value === "unknown") {
+            const mortyCharacters = characters.filter((character) => {
+              return (
+                character.name.includes("Morty") && character.status.includes("unknown")
+              );
+            });
+            mortyCharacters.forEach(renderCharacter);
+         } else if (
+            inputText.value.toLowerCase().includes("morty") &&
+            statusSelection.value === "all status") {
+            const mortyCharacters = characters.filter((character) => {
+              return (
+                character.name.includes("Morty")
+              );
+            });
+            mortyCharacters.forEach(renderCharacter);
+          }
+            
+      /* ===== conditions for Beth ===== */
       
+      if (
+        inputText.value.toLowerCase().includes("beth") &&
+        statusSelection.value === "alive"
+      ) {
+        const bethCharacters = characters.filter((character) => {
+          return (
+            character.name.includes("Beth") && character.status.includes("Alive")
+          );
+        });
+        bethCharacters.forEach(renderCharacter);
+      } else if (
+        inputText.value.toLowerCase().includes("beth") &&
+        statusSelection.value === "dead") {
+        const bethCharacters = characters.filter((character) => {
+          return (
+            character.name.includes("Beth") && character.status.includes("Dead")
+          );
+        });
+        bethCharacters.forEach(renderCharacter); 
+      } else if (
+          inputText.value.toLowerCase().includes("beth") &&
+          statusSelection.value === "unknown") {
+          const bethCharacters = characters.filter((character) => {
+            return (
+              character.name.includes("Beth") && character.status.includes("unknown")
+            );
+          });
+          bethCharacters.forEach(renderCharacter);
+       } else if (
+          inputText.value.toLowerCase().includes("beth") &&
+          statusSelection.value === "all status") {
+          const bethCharacters = characters.filter((character) => {
+            return (
+              character.name.includes("Beth")
+            );
+          });
+          bethCharacters.forEach(renderCharacter);
+        }
+
+      
+    /* ===== conditions for all cards ===== */
+
+    if (
+      inputText.value === "" &&
+      statusSelection.value === "alive"
+    ) {
+      const allCharactersAlive = characters.filter((character) => {
+        return (
+          character.status.includes("Alive")
+        );
+      });
+      allCharactersAlive.forEach(renderCharacter);
+    } else if (
+      inputText.value === "" &&
+      statusSelection.value === "dead") {
+      const allCharactersDead = characters.filter((character) => {
+        return (
+          character.status.includes("Dead")
+        );
+      });
+      allCharactersDead.forEach(renderCharacter); 
+    } else if (
+        inputText.value === "" &&
+        statusSelection.value === "unknown") {
+        const allUnknownCharacters = characters.filter((character) => {
+          return (
+            character.status.includes("unknown")
+          );
+        });
+        allUnknownCharacters.forEach(renderCharacter);
+     } else if (
+        inputText.value === "" &&
+        statusSelection.value === "all status") {
+        const allCharacters = characters.filter((character) => {
+          return (
+            character.status
+          );
+        });
+        allCharacters.forEach(renderCharacter);
+
+
 
       /* else if (inputText.value.toLowerCase().includes("morty")){    // berücksichtigt eintippen von groß und Kleinbuchstaben (z.B. RicK)
     const mortyCharacters = characters.filter(character => {
