4462e63627601835f3bab342ceafd63171987afc
Dennis Koch <denniskoch@Denniss-Air.fritz.box>
2022-03-27 10:01:31 +0200

    add filter

--- src/main.js
+++ src/main.js
@@ -1,31 +1,26 @@
-const baseUrl = 'https://rickandmortyapi.com/api/character?page=';
+const baseUrl = "https://rickandmortyapi.com/api/character?page=";
 const numPages = 5;
 
 const urls = Array(numPages)
-              .fill() // [undefined, undefined, ...]
-              .map((_, index) => baseUrl + (index + 1));
+  .fill() // [undefined, undefined, ...]
+  .map((_, index) => baseUrl + (index + 1));
 //_________Funktion Seitenanzahl____
-const promises = urls.map(url => fetch(url).then(res => res.json()))
+const promises = urls.map((url) => fetch(url).then((res) => res.json()));
 
-Promise
-  .all(promises)
-  .then(pages => {
-    const characters = pages.flatMap(page => page.results)
-    characters.forEach(renderCharacter)
-  })
+Promise.all(promises).then((pages) => {
+  const characters = pages.flatMap((page) => page.results);
+  characters.forEach(renderCharacter);
+});
 
-const upperList = document.querySelector(".cards")
+const upperList = document.querySelector(".cards");
 
-  function renderCharacter(character) {
-const cardContent = document.createElement('li')
-  const names = document.createElement('p')
-  cardContent.append(names)
+function renderCharacter(character) {
+  const cardContent = document.createElement("li");
+  const names = document.createElement("p");
+  cardContent.append(names);
   names.textContent = character.name;
-  const picture = document.createElement('img')
-  cardContent.append(picture)
+  const picture = document.createElement("img");
+  cardContent.append(picture);
   picture.src = character.image;
   upperList.append(cardContent);
 }
-
-
- 
